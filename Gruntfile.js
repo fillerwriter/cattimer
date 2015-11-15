b443746540aada5562e1a03fcbd0aeb7c1199261
Brandon Morrison <bmorrison@phase2technology.com>
2015-11-14 21:24:41 -0500

    Adjust template cache

--- Gruntfile.js
+++ Gruntfile.js
@@ -344,7 +344,7 @@ module.exports = function (grunt) {
           usemin: 'scripts/scripts.js'
         },
         cwd: '<%= yeoman.app %>',
-        src: 'views/{,*/}*.html',
+        src: 'app/scripts/{,*/}*.html',
         dest: '.tmp/templateCache.js'
       }
     },
